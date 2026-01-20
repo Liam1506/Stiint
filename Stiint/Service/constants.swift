e230c0f5799565fe3589450d3d5248d2af49ead5
Liam Wittig <liam.wittig@sap.com>
2026-01-20 09:32:48 +0100

    Removed flags

--- Stiint/Service/constants.swift
+++ Stiint/Service/constants.swift
@@ -5,10 +5,6 @@
 //  Created by Wittig, Liam on 22.12.25.
 //
 
-let BETA: Bool = true
-let DEV: Bool = false
-let PAYWALL: Bool = true
-
 let PRIVCY_POLICY_LINK: String = "https://www.stiint.liamwittig.de/privacy"
 let TERMS_OF_SERVICE_LINK: String = "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/"
 let HELP_LINK: String = "https://www.liamwittig.de/support"
