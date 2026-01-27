3588729630a4ca63dfe9d466293f8abeda159bd9
Liam Wittig <liam.wittig@sap.com>
2026-01-27 13:55:46 +0100

    Fixed dynamic island

--- stiintWidgetExtension/stiintWidgetExtension.swift
+++ stiintWidgetExtension/stiintWidgetExtension.swift
@@ -42,59 +42,95 @@ struct stiintWidgetExtension: Widget {
                 // Expanded regions
                 DynamicIslandExpandedRegion(.leading) {
                     // Image(systemName: context.attributes.icon).foregroundStyle(Color(hex:  context.attributes.color))
-
-                    VStack {
-                        Spacer()
-                        Image(systemName: context.attributes.icon)
-                            .font(.system(size: 30))
-                            .foregroundColor(Color(hex: context.attributes.color))
-                            .multilineTextAlignment(.center)
-                        Spacer()
-                    }
+                    HStack{
+                        VStack {
+                            Spacer()
+                            Image(systemName: context.attributes.icon)
+                                .font(.system(size: 30))
+                                .foregroundColor(
+                                    Color(hex: context.attributes.color)
+                                )
+                                .multilineTextAlignment(.center)
+                            Spacer()
+                        }
+                        VStack{
+                            Spacer()
+                            Text(context.attributes.name)
+                                .font(.headline)
+                                .padding(.bottom, 14)
+                                .lineLimit(1)
+                                .frame(maxWidth: 200, alignment: .leading)
+                                
+                        }
+                    }.frame(width: 200, height: 60)
                 }
                 DynamicIslandExpandedRegion(.trailing) {
                     
-                     VStack(alignment: .trailing){
-                         Spacer()
-                         Text(context.attributes.startTime, style: .timer)
-                             .font(.system(size: 100, weight: .light, design: .monospaced))
-                             .lineLimit(1)
-                             .minimumScaleFactor(0.01)
-                             .frame(width: 100, height: 50, alignment: .center)
-                         Spacer()
-                     }
+                    VStack(alignment: .trailing){
+                        Spacer()
+                        Text(context.attributes.startTime, style: .timer)
+                            .font(
+                                .system(
+                                    size: 100,
+                                    weight: .light,
+                                    design: .monospaced
+                                )
+                            )
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.01)
+                            .frame(width: 100, height: 50, alignment: .center)
+                        Spacer()
+                    }.frame(height: 60)
                      
                     /*
-                    Text(context.attributes.startTime, style: .timer)
-                        .font(.system(size: 100, weight: .light, design: .monospaced))
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.01)
-                        .frame(width: 100, height: 50, alignment: .center)
+                     Text(context.attributes.startTime, style: .timer)
+                     .font(.system(size: 100, weight: .light, design: .monospaced))
+                     .lineLimit(1)
+                     .minimumScaleFactor(0.01)
+                     .frame(width: 100, height: 50, alignment: .center)
                     
-                        .frame(maxHeight: .infinity, alignment: .center)
-                 */
+                     .frame(maxHeight: .infinity, alignment: .center)
+                     */
                 }
-                DynamicIslandExpandedRegion(.center) {
-                    HStack(alignment: .bottom) {
-                        Text(context.attributes.name)
-                            .font(.headline)
-                            .lineLimit(1)                   // single line
-                            .truncationMode(.tail)          // end with "..."
-                            .frame(width: 250, alignment: .leading)
-                            .minimumScaleFactor(0.01)       // scales down if needed
-                            .padding(.leading, 20)
-
-                        Spacer()
-                    }
-                    .frame(width: 250)
-                }
+                /*DynamicIslandExpandedRegion(.center) {
+                 Text(context.attributes.name)
+                 .font(.headline)
+                 .lineLimit(1)
+                 .minimumScaleFactor(0.7)
+                 // This frame aligns the text within the center block
+                 // without forcing the whole Island to expand.
+                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                 .padding(.leading, 12)
+                 .padding(.bottom, 4)
+                 .background(.green)
+                 }*/
+                /*DynamicIslandExpandedRegion(.center,priority: 2) {
+                 VStack (alignment: .leading){
+                        
+                        
+                 HStack(alignment: .bottom) {
+                 Text(context.attributes.name)
+                 .font(.headline)
+                 .lineLimit(1)                   // single line
+                 .truncationMode(.tail)          // end with "..."
+                 .frame( alignment: .leading)
+                 .minimumScaleFactor(0.01)       // scales down if needed
+                 .padding(.leading, 20)
+                 Spacer()
+                            
+                 }
+                 }
+                 .background(.green)
+                 }*/
             } compactLeading: {
-                Image(systemName: context.attributes.icon).foregroundStyle(Color(hex: context.attributes.color))
+                Image(systemName: context.attributes.icon)
+                    .foregroundStyle(Color(hex: context.attributes.color))
                     .font(.system(size: 17))
             } compactTrailing: {
                 EmptyView()
             } minimal: {
-                Image(systemName: context.attributes.icon).foregroundStyle(Color(hex: context.attributes.color))
+                Image(systemName: context.attributes.icon)
+                    .foregroundStyle(Color(hex: context.attributes.color))
                     .font(.system(size: 17))
             }
         }
